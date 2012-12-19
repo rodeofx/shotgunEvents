05176099abe24178720adbc742dcc2e324126c8c
Patrick Boucher <pboucher@patrickboucher.com>
2012-12-18 21:24:14 -0500

    Docstrings

--- src/shotgunEventDaemon.py
+++ src/shotgunEventDaemon.py
@@ -1006,10 +1006,16 @@ class CustomSMTPHandler(logging.handlers.SMTPHandler):
 
 
 class EventDaemonError(Exception):
+    """
+    Base error for the Shotgun event system.
+    """
     pass
 
 
 class ConfigError(EventDaemonError):
+    """
+    Used when an error is detected in the config file.
+    """
     pass
 
 
